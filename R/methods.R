ef577d2938710beb62d7c2a4c6c93705916a50d9
Dusty Gannon <dustygannon@MACKLEMORE.forestry.oregonstate.edu>
2024-05-29 11:56:53 -0700

    summary is ready, but still needs predict

--- R/methods.R
+++ R/methods.R
@@ -1,9 +1,16 @@
 
 
+#' Summarize fitted isoniche model
+#'
+#' @param mfit A fitted isoniche object
+#'
+#' @return This general method returns a summary of the results of the fitted model, including
+#' 95% credible intervals for model coefficients as well as Gelman's Rhat statistic for
+#' estimating chain convergence.
+#' @export
+#'
 summary.isoniche <- function(mfit){
 
-  # get the call
-  call <- deparse(substitute(summary(mfit)))
   # get group names
   resp_names <- unlist(lapply(
     mfit$model$mean,
@@ -34,9 +41,11 @@ summary.isoniche <- function(mfit){
   )
 
   joint_post <- rstan::extract(mfit$fit)
+  rhat_all <- rstan::summary(mfit$fit)$summary[, "Rhat"]
 
   # remove lp from list
   joint_post <- joint_post[-length(joint_post)]
+  rhat <- rhat_all[-length(rhat_all)]
 
   # add stats-like names
   for(i in 1:length(mparnames)){
@@ -50,7 +59,7 @@ summary.isoniche <- function(mfit){
   parnames <- c(
     paste("mean", resp_names, sep = "_"),
     paste("sd", resp_names, sep = "_"),
-    "cor"
+    "corr"
   )
   df_sum <- data.frame(
     coefficient = unlist(lapply(joint_post, colnames)),
@@ -69,27 +78,90 @@ summary.isoniche <- function(mfit){
       function(x){
         apply(x, 2, quantile, probs = 0.975)
       }
-    ))
+    )),
+    Rhat = rhat
   )
 
-  if(interactive()){
+  # compile the return list
+  isosum <- list(
+    model = list(
+      model_mean = mfit$model$mean,
+      model_var = mfit$model$var[1:2],
+      model_corr = mfit$model$var[3]
+    ),
+    note = "Note: sd_ coefficients are on the log scale and correlation coefficients are on a modified logit scale.\n",
+    summary = df_sum
+  )
+
+  class(isosum) <- "summary.isoniche"
+  isosum
+}
+
+
+#
+# predict.isoniche <- function(mfit, newdat, summarize = FALSE){
+#
+# }
+#
+
+
+
+#' Print methods for the summarized isoniche object
+#'
+#' @param sumobj summary.isoniche object
+#' @param digits Number of digits to include in printed table. Defaults to 3.
+#'
+#' @export
+#'
+print.summary.isoniche <- function(sumobj, digits = 3){
     df_sum_print <- sapply(
-      1:ncol(df_sum),
+      1:ncol(sumobj$summary),
       function(i, df){
         if(is.double(df[,i])){
-          return(round(df[,i], 2))
+          return(round(df[,i], digits))
         } else{
           return(df[,i])
         }
       },
-      df = df_sum
+      df = sumobj$summary
     )
     df_sum_print <- as.data.frame(df_sum_print)
-    names(df_sum_print) <- names(df_sum)
-    print(df_sum_print)
-    cat("Note: sd_ coefficients are on the log scale and correlation coefficients are on a modified logit scale.\n")
-  }
+    names(df_sum_print) <- names(sumobj$summary)
 
-  invisible(df_sum)
+    # first mean model
+    cat(
+      c(
+        "Model for mean vector:",
+        as.character(unlist(sumobj$model$model_mean)),
+        "link: identity\n"
+      ),
+      sep = "\n"
+    )
+
+    # print sd model
+    cat(
+      c(
+        "Model for scale (SD) vector:",
+        as.character(unlist(sumobj$model$model_var)),
+        "link: log()\n"
+      ),
+      sep = "\n"
+    )
+
+    # print model for correlation
+    cat(
+      c(
+        "Model for correlation:",
+        as.character(unlist(sumobj$model$model_corr)),
+        "link: logit(0.5 * (sigma + 1))\n"
+      ),
+      sep = "\n"
+    )
+
+    print(df_sum_print)
 
 }
+
+
+
+
